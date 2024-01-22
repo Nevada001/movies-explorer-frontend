048cb0c17314c6c15123872bf018f19bac0be780
Nevada <olis0001@yandex.ru>
2024-01-22 11:21:43 +0300

    feat: remake link 'back'

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -4,11 +4,12 @@ import "./NotFound.css";
 export default function NotFound() {
 
 
+
   return (
     <section className="notFound">
       <h2 className="notFound__title">404</h2>
       <p className="notFound__text">Страница не найдена</p>
-      <Link className="notFound__link" to={-1}>Назад</Link>
+      <Link className="notFound__link" to={-4}>Назад</Link>
     </section>
   );
 }
